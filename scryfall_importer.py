85fbd910da9feecbddb9ccf49afe9e9e992df5a2
Xedrix <34930703+xedrix@users.noreply.github.com>
2022-05-28 22:57:07 -0400

    Added validation docstrings and changed method of filtering from loc to isin

--- scryfall_importer.py
+++ scryfall_importer.py
@@ -1,25 +1,15 @@
-import requests
 import json
+import glob
+import os
 import pandas as pd
 import datetime as dt
 
 SCRYFALL_ENDPOINT = "https://c2.scryfall.com/file/scryfall-bulk/default-cards/default-cards-20220521090423.json"
 
 
-def ingest_scryfall_bulk_data(date):
-    # TODO: when running this on schedule, add check to only ingest new data since last ingest. Use "released_at" field?
-    response = requests.get(url=SCRYFALL_ENDPOINT)
-    response.raise_for_status()
-    data = response.json()
-    with open(file=f"scryfall_bulk_imports/{date}_raw.json", mode="w", encoding="utf8") as f:
-        json.dump(data, f, ensure_ascii=True)
-
-
-def clean_ingest_bulk_data(date):
-    # Takes raw json from Scryfall and drops all un-needed fields and all online-only cards
-    with open(file=f"scryfall_bulk_imports/{date}_raw.json", mode="r", encoding="utf8") as f:
-        df = pd.read_json(f)  # file is raw from Scryfall and includes many unwanted fields
-
+def ingest_scryfall_bulk_data():
+    """Queries Scryfall API for list of cards. Drops un-necessary columns and saves list as new JSON file."""
+    df = pd.read_json(SCRYFALL_ENDPOINT)
     slim_dataframe = df.loc[:, ["id", "name", "foil", "nonfoil", "set", "set_name", "collector_number", "digital"]]
     temp_dict = slim_dataframe.to_dict("records")  # converts to dict for efficiency
     digital_cards = [row for row in temp_dict if row["digital"] == False]
@@ -28,14 +18,47 @@ def clean_ingest_bulk_data(date):
         json.dump(digital_cards, f, ensure_ascii=True)
 
 
-search = {
-    "name": "Fury Sliver",
-    "foil": "true",
-    "set": "tsp",
-    "collector_number": 157
-}
+# example of cards for testing. Includes all fields required to compare with scryfall
+list_of_cards = [
+    {
+        "name": "Fury Sliver",
+        "foil": "false",
+        "set": "tsp",
+        "collector_number": 157
+    },
+    {
+        "name": "Queen Marchesa",
+        "foil": "true",
+        "set": "cn2",
+        "collector_number": 78
+    }
+]
+for card in list_of_cards:
+    card["foil"].capitalize()
+    card["foil"] = bool(card["foil"])
 
-with open(file=f"scryfall_bulk_imports/{dt.datetime.today()}_clean.json", mode="r") as file:
-    for key, value in search:
-        pass
 
+def validate_cards(list_of_cards):
+    """checks list of cards and finds row that contains same name, set, collector number and if the card can be foil
+    or non-foil. Returns True or False for each card."""
+    file_list = glob.glob("scryfall_bulk_imports/*")
+    latest_file = max(file_list, key=os.path.getctime)
+    with open(file=latest_file, mode="r") as file:
+        df = pd.read_json(file)
+    result = []
+    for card in list_of_cards:
+        if card["foil"]:
+            if df.name.isin([card["name"]]) and df.foil == True and df.set.isin([card["set"]]) and df.collector_number.isin([card["collector_number"]]):
+                result.append(True)
+            else:
+                result.append(False)
+        else:
+            if df.loc[(df.name == card["name"]) & (df.nonfoil == True) & (df.set == card["set"])
+                      & (df.collector_number == card["collector_number"])]:
+                result.append(True)
+            else:
+                result.append(False)
+    return result
+
+
+print(validate_cards(list_of_cards))
